6e7f0d8f12f9fb70e0f5571f8add005493799c0d
siemensikkema <siemensikkema@users.noreply.github.com>
2021-09-29 11:48:58 +0200

    Split up test classes into files

--- Tests/DecodedTests/PropertyDecodingTests.swift
+++ Tests/DecodedTests/PropertyDecodingTests.swift
@@ -1,35 +1,6 @@
 import Decoded
 import XCTest
 
-final class TopLevelValueDecodingTests: XCTestCase {
-    func test_value() throws {
-        let decoded: Decoded<Int> = try decode("1")
-        XCTAssertEqual(decoded.codingPath, [])
-        XCTAssertEqual(decoded.result, .success(.value(1)))
-        XCTAssertEqual(decoded.value, 1)
-    }
-
-    func test_expected_nil() throws {
-        let decoded: Decoded<Int?> = try decode("null")
-        XCTAssertEqual(decoded.codingPath, [])
-        XCTAssertEqual(decoded.result, .success(.nil))
-        XCTAssertNil(try decoded.unwrapped)
-    }
-
-    func test_unexpected_nil() throws {
-        let decoded: Decoded<Int> = try decode("null")
-        XCTAssertEqual(decoded.codingPath, [])
-
-        guard case .failure(let failure) = decoded.result else {
-            XCTFail("")
-            return
-        }
-        XCTAssertEqual(failure.errorType, .valueNotFound)
-        XCTAssertEqual(failure.debugDescription, "Expected Int but found null value instead.")
-        XCTAssertThrowsError(try decoded.unwrapped)
-    }
-}
-
 final class PropertyDecodingTests: XCTestCase {
     struct OptionalName: Decodable {
         let name: Decoded<String?>
